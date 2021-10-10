883d11feb4205477c2758f80414ccfda9ca8456a
Daniya Niazi <farihadania@hotmail.com>
2021-10-11 00:01:35 +0500

    feat: responsive navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import {
   HomeOutlined,
   BulbOutlined,
   FundOutlined,
   MoneyCollectOutlined,
+  MenuOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import icon from "../images/cryptocurrency.png";
 
 const NavBar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+  // GET WIDTH OF SCREEN
+  const handleResize = () => {
+    setScreenSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return window.addEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -17,22 +38,45 @@ const NavBar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">CryptoVerse</Link>
         </Typography.Title>
+        <Button
+          shape="circle"
+          type="primary"
+          size="large"
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item
+            icon={<HomeOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item
+            icon={<FundOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
 
-        <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
+          <Menu.Item
+            icon={<MoneyCollectOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item
+            icon={<BulbOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   );
 };
